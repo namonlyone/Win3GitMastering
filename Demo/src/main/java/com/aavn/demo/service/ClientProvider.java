54b821020ec88f7c67f413496dfb8b116d851a36
lqdung <lqdung@DN-AAVN-WS-148.aavn.local>
2014-03-13 09:48:00 +0700

    updated

--- Demo/src/main/java/com/aavn/demo/service/ClientProvider.java
+++ Demo/src/main/java/com/aavn/demo/service/ClientProvider.java
@@ -2,11 +2,7 @@ package com.aavn.demo.service;
 
 import org.elasticsearch.client.Client;
 import org.elasticsearch.client.transport.TransportClient;
-import org.elasticsearch.common.settings.ImmutableSettings;
-import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.transport.InetSocketTransportAddress;
-import org.elasticsearch.node.Node;
-
 /**
  * 
  * @author lqdung
@@ -15,14 +11,11 @@ import org.elasticsearch.node.Node;
 public class ClientProvider {
     
     private static ClientProvider instance = null;
-    private static Object lock      = new Object();
+    private static Object lock = new Object();
+	private Client client;
     
-    private Client client;
-    private Node node;
-
     public static ClientProvider instance(){
-        
-        if(instance == null) { 
+    	if(instance == null) { 
             synchronized (lock) {
                 if(null == instance){
                     instance = new ClientProvider();
@@ -33,23 +26,12 @@ public class ClientProvider {
     }
 
     public void prepareClient(){
-        Settings settings = ImmutableSettings.settingsBuilder().put("cluster.name", "dungle").build();
-    	client = new TransportClient(settings).addTransportAddress(new InetSocketTransportAddress("localhost", 9300));
+        //Settings settings = ImmutableSettings.settingsBuilder().put("cluster.name", "dungle").build();
+    	client = new TransportClient().addTransportAddress(new InetSocketTransportAddress("localhost", 9300));
     }
-
-    public void closeNode(){
-        
-        if (!node.isClosed()) {
-        	node.close();
-        }
-    }
+    
     
     public Client getClient(){
         return client;
-    }
-    
-    public void printThis() {
-        System.out.println(this);
-    }
-    
+    }       
 }
