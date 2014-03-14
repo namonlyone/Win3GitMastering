40e619d1ffdaae3fb6445ee64c62945b7ef1a4ad
nvhoang2 <nvhoang2@dn-aavn-ws-149.aavn.local>
2014-03-14 10:32:38 +0700

    update

--- Demo/src/main/java/com/aavn/demo/service/Neo4jDataWriter.java
+++ Demo/src/main/java/com/aavn/demo/service/Neo4jDataWriter.java
@@ -1,9 +1,7 @@
 package com.aavn.demo.service;
 
-import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import java.util.Properties;
 
 import org.apache.commons.lang3.StringUtils;
